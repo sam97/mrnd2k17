f7497f5afa169e984578bfc825ead67e5ba95307
sam97 <smahaseeb@hotmail.com>
2017-10-21 00:16:31 +0530

    Skeletal design.
    Split page into four parts:
    - DateHeader
    - Timeline
    - DayDetails
    - Footer.
    All empty for now.
    Remove App.css

---
+++ src/DateHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DateHeader extends Component {
+	render() {
+		return (
+			<div className="dateheader">
+				
+			</div>
+		)
+	}
+}
+
+export default DateHeader;
---
+++ src/DayDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DayDetails extends Component {
+	render() {
+		return (
+			<div className="daydetails">
+				
+			</div>
+		)
+	}
+}
+
+export default DayDetails;
