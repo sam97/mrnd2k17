b265cf1d7ec2f1aea0285220eb368c0b1515c4b1
sam97 <smahaseeb@hotmail.com>
2017-10-23 23:32:08 +0530

    Skeletal Timeline with connection to DateHeader.
    - Make timeline a ul with each time node as an 'X' in an li tag.
    - Add some CSS (documented).
    - New definition of the data store.
    - Change Apps state representation.
    - Add mouseover, mouseout and click handling functions.
    - Correct typo in index.css

--- src/App.js
+++ src/App.js
@@ -10,19 +10,67 @@ class App extends Component {
 		super(props);
 
 		/* Supposed to be the data store.  */
+		var timeline = [
+			{
+				date: new Date(2017, 9, 1),
+				desc: "somethin' happened on the 1st",
+				personalNote: "personell stuff, m8, bugger off"
+			},
+			{
+				date: new Date(2017, 9, 2),
+				desc: "somethin' happened on the 2nd",
+				personalNote: "personell stuff, m8, bugger off"
+			},
+			{
+				date: new Date(2017, 9, 3),
+				desc: "somethin' happened on the third",
+				personalNote: "personell stuff, m8, bugger off"
+			},
+			{
+				date: new Date(2017, 9, 4),
+				desc: "somethin' happened on the fourth",
+				personalNote: "personell stuff, m8, bugger off"
+			}
+		];
+		
 		this.state = {
-			presentDay: new Date()
+			presentDay: timeline[0],
+			headerDate: timeline[0].date,
+			timeline: timeline
 		}
+	}
+
+	onTimelineMouseOver(newDate) {
+		this.setState({
+			headerDate: newDate
+		})
+	}
+
+	onTimelineMouseOut() {
+		this.setState(prevState => {
+			return {
+				headerDate: prevState.presentDay.date
+			}
+		})
+	}
+
+	onTimelineClick(newDay) {
+		this.setState({
+			presentDay: newDay
+		})
 	}
 
 	render() {
 		return (
 			// <div>
 				[
-					<DateHeader key={1} date={this.state.presentDay} />,
-					(<Timeline key={2}>
-
-					</Timeline>),
+					<DateHeader key={1} date={this.state.headerDate} />,
+					<Timeline key={2}
+					          timeline={this.state.timeline}
+					          onMouseOver={newDate => this.onTimelineMouseOver(newDate)}
+							  onMouseOut={() => this.onTimelineMouseOut()}
+							  onClick={newDay => this.onTimelineClick(newDay)}
+					 />,
 					(<DayDetails key={3}>
 
 					</DayDetails>),
--- src/Timeline.js
+++ src/Timeline.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import './Timeline.css'
+
+function TimeNode(props) {
+	return (
+		<li>
+			<span onMouseOver={props.onMouseOver}
+				  onMouseOut={props.onMouseOut}
+				  onClick={props.onClick}
+			>
+				X
+			</span>
+		</li>
+	);
+}
 
 class Timeline extends Component {
+	// eslint-disable-next-line
+	constructor(props) {
+		super(props);
+
+	}
+
 	render() {
 		return (
 			<div className="timeline">
-				
+				<ul>
+					{this.props.timeline.map(day => {
+						return (
+							<TimeNode onMouseOver={() => this.props.onMouseOver(day.date)}
+							          onMouseOut={this.props.onMouseOut}
+							          onClick={() => this.props.onClick(day)}
+							/>
+						);
+					})}
+				</ul>
 			</div>
 		)
 	}
